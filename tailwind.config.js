036477861a17989ab7f463f1c2613c06f409026f
Indra <indraahmadfirdaus@gmail.com>
2024-12-07 13:27:17 +0700

    fix: tailwind es modules config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+import daisyui from 'daisyui';
+
 export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
-export const plugins = [require('daisyui')];
-export const daisyui = {
-    themes: ["light", "dark", "lofi", "dracula", "synthwave"],
+export const plugins = [daisyui];
+export const daisyui = {    themes: ["light", "dark", "lofi", "dracula", "synthwave"],
 };
   
